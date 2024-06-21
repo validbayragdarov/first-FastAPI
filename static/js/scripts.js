2e52047a7f2f60ab0748e4f8e077e64ed1187d65
validbayragdarov <validwe@gmail.com>
2024-06-21 11:40:08 +0300

    added find by id and by name

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,6 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const createUserForm = document.getElementById('create-user-form');
     const loadUsersButton = document.getElementById('load-users');
     const usersList = document.getElementById('users');
+    const getUserByIdForm = document.getElementById('get-user-by-id-form');
+    const userByIdResult = document.getElementById('user-by-id-result');
+    const getUserByNameForm = document.getElementById('get-user-by-name-form');
+    const userByNameResult = document.getElementById('user-by-name-result');
 
     createUserForm.addEventListener('submit', async (e) => {
         e.preventDefault();
@@ -38,6 +42,44 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         } else {
             alert('Error loading users.');
+        }
+    });
+
+    getUserByIdForm.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        const userId = document.getElementById('user-id').value;
+
+        const response = await fetch(`/users/user/${userId}`);
+        const result = await response.json();
+
+        if (result.message === 'success') {
+            userByIdResult.innerHTML = '';
+            result.users.forEach(user => {
+                const li = document.createElement('li');
+                li.textContent = `ID: ${user.id}, Name: ${user.name}, Email: ${user.email}`;
+                userByIdResult.appendChild(li);
+            });
+        } else {
+            alert('Error fetching user by ID.');
+        }
+    });
+
+    getUserByNameForm.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        const userName = document.getElementById('user-name').value;
+
+        const response = await fetch(`/users/user?user_name=${userName}`);
+        const result = await response.json();
+
+        if (result.message === 'success') {
+            userByNameResult.innerHTML = '';
+            result.users.forEach(user => {
+                const li = document.createElement('li');
+                li.textContent = `Name: ${user.name}, Email: ${user.email}`;
+                userByNameResult.appendChild(li);
+            });
+        } else {
+            alert('Error fetching user by name.');
         }
     });
 });
